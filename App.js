09a225bb73bd1eb98c0d9c9598ffb8724e5de6b1
siddhesh494 <siddhesh.ss26@gmail.com>
2023-11-14 10:21:56 +0530

    lec-3

--- App.js
+++ App.js
@@ -1,32 +1,62 @@
 import React from "react"
-import ReactDOM from "react-dom/client" // As react-dom give warning
-// Here we are just creating a element inside react, 
-// So this is a core react job
+import ReactDOM from "react-dom/client"
+import Logo from './Images/js-logo.png'
+import User from './Images/user-logo.png'
 
-/*
-<div id="parent">
-  <div id="child">
-    <h1>I'm an h1 tag</h1>
-    <h2>I'm an h2 tag</h2>
-  </div>
-</div>
-*/
-const parent = React.createElement(
-  "div", 
-  {id: "parent"}, 
-  React.createElement(
-    "div",
-    {id: "child"},
-    [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")
-    ]
-  )
+const title = (
+  <h1 className="head">
+    Namaste React using JSX
+  </h1>
 )
-console.log(parent)
-// We have to create a root inside react to do all the DOM operations
-// To create a root in react, it is a job of react DOM
+
+const Title = () => (
+  <h1 className="head">
+    Namaste React using JSX
+  </h1>
+)
+
+const HeadersComponent = () => {
+  return (
+    <div 
+      className="header-bar"
+    > 
+      <div>
+        <img 
+          src={Logo}
+          width={50}
+        />
+      </div>
+      <div>
+        <input type="text" placeholder="Search.." />
+      </div>
+      <div>
+        <img 
+          src={User}
+          width={50}
+        />
+      </div>
+    </div>
+  )
+}
+
+// React components
+const ContentComponent = () => {
+  return (
+    <div>
+      <HeadersComponent />
+      {/* This is component composition */}
+      {Title()} 
+      <h1>
+        Namanste React Functional components
+      </h1>
+    </div>
+    
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// Now we just have to render the root with the heading
-root.render(parent)
+// Rendering react functional component
+root.render(
+  <ContentComponent />
+)
